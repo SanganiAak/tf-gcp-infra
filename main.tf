54243ca8f700fbdd498fee9be7030263a0627e61
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-02-15 13:02:09 -0500

    formated

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # VPC
 resource "google_compute_network" "vpc" {
-  count                   = var.var_count
-  name                    = "${var.vpc_name}-${count.index}"
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  count                           = var.var_count
+  name                            = "${var.vpc_name}-${count.index}"
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
@@ -19,16 +19,16 @@ resource "google_compute_subnetwork" "webapp" {
 resource "google_compute_subnetwork" "db" {
   count         = var.var_count
   name          = "${var.database}-${count.index}"
-  ip_cidr_range = "10.0.${count.index + 100}.0/24" 
+  ip_cidr_range = "10.0.${count.index + 100}.0/24"
   region        = var.region
   network       = google_compute_network.vpc[count.index].id
 }
 
 # Route for webapp subnet
 resource "google_compute_route" "webapp_route" {
-  count          = var.var_count
-  name           = "webapp-route-${count.index}"
-  dest_range     = "0.0.0.0/0"
-  network        = google_compute_network.vpc[count.index].id
+  count            = var.var_count
+  name             = "webapp-route-${count.index}"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.vpc[count.index].id
   next_hop_gateway = "default-internet-gateway"
 }
--- variable.tf
+++ variable.tf
@@ -12,16 +12,16 @@ variable "vpc_name" {
 }
 
 variable "var_count" {
-  default     = 1
-  type        = number
+  default = 1
+  type    = number
 }
 
 variable "app_name" {
-  type = string
+  type    = string
   default = "webapp"
 }
 variable "database" {
-  type = string
+  type    = string
   default = "db"
-  
+
 }
