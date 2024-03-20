5b8f8d98d2e50b3a424f909b675af5ca63189457
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-03-20 03:38:29 -0400

    log writer service account binding

--- serviceaccount.tf
+++ serviceaccount.tf
@@ -6,14 +6,13 @@ resource "google_service_account" "service_account" {
 # Assign the Logging Admin role at the project level
 resource "google_project_iam_binding" "logging_admin" {
   project = var.project_id
-  role    = "roles/logging.configWriter"
+  role    = "roles/logging.logWriter"
 
   members = [
     "serviceAccount:${google_service_account.service_account.email}",
   ]
 }
 
-# Assign the Monitoring Metrics Writer role at the project level
 resource "google_project_iam_binding" "monitoring_metrics_writer" {
   project = var.project_id
   role    = "roles/monitoring.metricWriter"
