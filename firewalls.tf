162c86410881a27d95d84c86123c1f32710cf19d
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-04-10 14:38:48 -0400

    variablizing values and removing unused code

--- firewalls.tf
+++ firewalls.tf
@@ -20,24 +20,10 @@ resource "google_compute_firewall" "deny_ssh" {
   direction = "INGRESS"
   priority  = 1000
 
-  deny {
+  allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
-
-# resource "google_compute_firewall" "database_allow" {
-#   name      = "database-allow-port"
-#   network   = google_compute_network.vpc.id
-#   direction = "INGRESS"
-#   priority  = 1000
-
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["3306"]
-#   }
-
-#   source_ranges = ["0.0.0.0/0"]
-# }
